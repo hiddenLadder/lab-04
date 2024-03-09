85619ed29960a044d22dcaf50aa4dffd465c734e
hiddenLadder <kirill4ernega@gmail.com>
2024-03-09 11:21:38 +0800

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let arr = [1, 1, 2, 10, 3, 3, 3, 66, 0, 2, 3, 23, 2, 3];
+
+    let mut counts = HashMap::new();
+
+    for num in arr {
+        let count = counts.entry(num).or_insert(0);
+        *count += 1;
+    }
+
+    let mut sorted_counts = counts.drain().collect::<Vec<_>>();
+    sorted_counts.sort_by_key(|&(key, _)| key);
+
+    for (num, count) in sorted_counts {
+        println!("Number {}: {}", num, count);
+    }
 }
